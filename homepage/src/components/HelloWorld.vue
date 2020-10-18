b6a2c4bff3423115815cf2f3fe06aaa85da94545
Stefanie S <git@s-blu.de>
2020-10-18 12:42:23 +0200

    feat(homepage): add i18n package

--- homepage/src/components/HelloWorld.vue
+++ homepage/src/components/HelloWorld.vue
@@ -15,6 +15,8 @@
         </li>
       </ul>
     </div>
+
+    <div>translated: {{ $t("message") }}</div>
   </div>
 </template>
 
--- homepage/src/main.ts
+++ homepage/src/main.ts
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./../src/assets/styles/pure-min.css";
 import "./../src/assets/styles/grids-responsive-min.css";
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount("#app");
